f83ac4a0cc94b2f6ada996bd8f8ab866dd2a1a0e
tvStatic <tvStatic@users.noreply.github.com>
2019-06-27 09:32:34 +1000

    Fallback to sqlite3 before memory

--- src/components/index/index.js
+++ src/components/index/index.js
@@ -1,12 +1,5 @@
 const _ = require('lodash')
-
-// better-sqlite3 may not be present - fallback to in memory processing
-try {
-  var Database = require('better-sqlite3')
-} catch (err) {
-  var Database = null
-}
-
+const async = require('async')
 const delta = require('./delta')
 const EventEmitter = require('events')
 const exiftool = require('../exiftool/parallel')
@@ -20,15 +13,36 @@ const EXIF_DATE_FORMAT = 'YYYY:MM:DD HH:mm:ssZ'
 
 class Index {
   constructor (indexPath) {
+    this.indexPath = indexPath
+
     // create the database if it doesn't exist
     fs.mkdirpSync(path.dirname(indexPath))
 
-    if (Database) {
-      this.db = new Database(indexPath, {})
-      this.db.exec('CREATE TABLE IF NOT EXISTS files (path TEXT PRIMARY KEY, timestamp INTEGER, metadata BLOB)')
-    } else {
-      this.localDB = [];
-      warn("better-sqlite3 is not available - storing in memory instead")
+    this.initialise()
+  }
+
+  initialise () {
+    var Database
+    var sqlite3
+
+    // better-sqlite3 may not be present - fallback to in memory processing
+    try {
+      Database = require('better-sqlite3')
+      this.db = new Database(this.indexPath, {})
+      this.usingBetterSQLite3 = true
+    } catch (err) {
+      // try sqlite3
+      try {
+        sqlite3 = require('sqlite3')
+        this.db = new sqlite3.Database(this.indexPath)
+        this.usingSQLite3 = true
+        warn('better-sqlite3 is not available - using sqlite3 instead')
+      } catch (err) {
+        // use in memory map
+        this.localDB = []
+        this.usingInMemory = true
+        warn('better-sqlite3 is not available - storing in memory instead')
+      }
     }
   }
 
@@ -38,101 +52,271 @@ class Index {
   update (mediaFolder, options = {}) {
     // will emit many different events
     const emitter = new EventEmitter()
-    var index = this;
-
-    // prepared database statements
-    if (this.db) {
-      var selectStatement = this.db.prepare('SELECT path, timestamp FROM files')
-      var insertStatement = this.db.prepare('INSERT OR REPLACE INTO files VALUES (?, ?, ?)')
-      var deleteStatement = this.db.prepare('DELETE FROM files WHERE path = ?')
-      var countStatement = this.db.prepare('SELECT COUNT(*) AS count FROM files')
-      var selectMetadata = this.db.prepare('SELECT * FROM files')
-    }
-
-    // create hashmap of all files in the database
+    const index = this
+    const createTableSQL = 'CREATE TABLE IF NOT EXISTS files (path TEXT PRIMARY KEY, timestamp INTEGER, metadata BLOB)'
+    const selectStatementSQL = 'SELECT path, timestamp FROM files'
+    const countStatementSQL = 'SELECT COUNT(*) AS count FROM files'
+    const insertStatementSQL = 'INSERT OR REPLACE INTO files VALUES (?, ?, ?)'
+    const deleteStatementSQL = 'DELETE FROM files WHERE path = ?'
+    const selectMetadataSQL = 'SELECT * FROM files'
     const databaseMap = {}
-    if (selectStatement) {
-      for (var row of selectStatement.iterate()) {
-        databaseMap[row.path] = row.timestamp
+
+    var initialise
+    var emitRows
+    var countRows
+    var populateDatabaseMap
+    var deleteRows
+    var insertRow
+
+    function useBetterSQLite () {
+      var selectMetadata
+      var countStatement
+      var selectStatement
+      var deleteStatement
+      var insertStatement
+
+      initialise = function (callback) {
+        index.db.exec(createTableSQL)
+
+        selectMetadata = index.db.prepare(selectMetadataSQL)
+        countStatement = index.db.prepare(countStatementSQL)
+        selectStatement = index.db.prepare(selectStatementSQL)
+        deleteStatement = index.db.prepare(deleteStatementSQL)
+        insertStatement = index.db.prepare(insertStatementSQL)
+
+        callback()
       }
-    }
 
-    function finished () {
-      var result
-
-      // emit every file in the index
-      function emitRow(row) {
-        emitter.emit('file', {
-          path: row.path,
-          timestamp: new Date(row.timestamp),
-          metadata: JSON.parse(row.metadata)
-        })
-      }
-
-      if (selectMetadata) {
+      emitRows = function (callback) {
         for (var row of selectMetadata.iterate()) {
           emitRow(row)
         }
-      } else {
-        index.localDB.forEach(emitRow);
-      }
-      
-      // emit the final count
-      if (countStatement) {
-        result = countStatement.get()
-      } else {
-        result = index.localDB.length
+
+        callback()
       }
 
-      emitter.emit('done', { count: result.count })
+      countRows = function (callback) {
+        callback(countStatement.get().count)
+      }
+
+      populateDatabaseMap = function (callback) {
+        for (var row of selectStatement.iterate()) {
+          databaseMap[row.path] = row.timestamp
+        }
+
+        callback()
+      }
+
+      deleteRows = function (toDelete, callback) {
+        _.each(toDelete, path => {
+          deleteStatement.run(path)
+        })
+
+        callback()
+      }
+
+      insertRow = function (entry, timestamp, callback) {
+        insertStatement.run(entry.SourceFile, timestamp, JSON.stringify(entry))
+        callback()
+      }
     }
 
-    // find all files on disk
-    globber.find(mediaFolder, options, (err, diskMap) => {
-      if (err) return console.error('error', err)
+    function useInMemory (callback) {
+      initialise = function () {
+        callback()
+      }
 
-      // calculate the difference: which files have been added, modified, etc
-      const deltaFiles = delta.calculate(databaseMap, diskMap)
-      emitter.emit('stats', {
-        unchanged: deltaFiles.unchanged.length,
-        added: deltaFiles.added.length,
-        modified: deltaFiles.modified.length,
-        deleted: deltaFiles.deleted.length,
-        total: Object.keys(diskMap).length
-      })
+      emitRows = function (callback) {
+        index.localDB.forEach(emitRow)
+        callback()
+      }
 
-      // remove deleted files from the DB
-      if (deleteStatement) {
-        _.each(deltaFiles.deleted, path => {
-          deleteStatement.run(path)
+      countRows = function (callback) {
+        callback(index.localDB.length)
+      }
+
+      populateDatabaseMap = function (callback) {
+        index.localDB.forEach(function (row) {
+          databaseMap[row.path] = row.timestamp
+        })
+
+        callback()
+      }
+
+      deleteRows = function (toDelete, callback) {
+        _.each(toDelete, path => {
+          var foundIndex = index.localDB.findIndex(row => {
+            return row.path === path
+          })
+          if (foundIndex !== -1) {
+            index.localDB.splice(foundIndex, 1)
+          }
+        })
+
+        callback()
+      }
+
+      insertRow = function (entry, timestamp, callback) {
+        index.localDB.push({
+          key: entry.SourceFile,
+          timestamp: timestamp,
+          metadata: JSON.stringify(entry)
+        })
+
+        callback()
+      }
+    }
+
+    function useSQLite () {
+      var selectMetadata
+      var countStatement
+      var deleteStatement
+      var selectStatement
+      var insertStatement
+
+      initialise = function (callback) {
+        index.db.serialize(function () {
+          index.db.exec(createTableSQL)
+
+          selectMetadata = index.db.prepare(selectMetadataSQL)
+          countStatement = index.db.prepare(countStatementSQL)
+          deleteStatement = index.db.prepare(deleteStatementSQL)
+          selectStatement = index.db.prepare(selectStatementSQL)
+          insertStatement = index.db.prepare(insertStatementSQL, callback)
         })
       }
 
-      // check if any files need parsing
-      var processed = 0
-      const toProcess = _.union(deltaFiles.added, deltaFiles.modified)
-      if (toProcess.length === 0) {
-        return finished()
+      emitRows = function (callback) {
+        index.db.serialize(function () {
+          selectMetadata.each((err, row) => {
+            emitRow(row)
+          }, callback)
+        })
       }
 
-      // call <exiftool> on added and modified files
-      // and write each entry to the database
-      const stream = exiftool.parse(mediaFolder, toProcess, options.concurrency)
-      stream.on('data', entry => {
-        const timestamp = moment(entry.File.FileModifyDate, EXIF_DATE_FORMAT).valueOf()
+      countRows = function (callback) {
+        var ret
+        index.db.serialize(function () {
+          countStatement.get((err, row) => {
+            callback(row.count)
+          })
+        })
+      }
 
-        if (insertStatement) {
-          insertStatement.run(entry.SourceFile, timestamp, JSON.stringify(entry))
-        } else {
-          this.localDB.push({
-            key: entry.SourceFile, 
-            timestamp: timestamp, 
-            metadata: JSON.stringify(entry)
+      populateDatabaseMap = function (callback) {
+        index.db.serialize(function () {
+          selectStatement.each((err, row) => {
+            databaseMap[row.path] = row.timestamp
+          }, callback)
+        })
+      }
+
+      deleteRows = function (toDelete, callback) {
+        index.db.serialize(function () {
+          var todo = []
+
+          function deleteRow (path) {
+            return function (callback) {
+              deleteStatement.run(path, () => {
+                callback()
+              })
+            }
+          }
+
+          _.each(toDelete, path => {
+            todo.push(deleteRow(path))
+          })
+
+          async.series(todo, callback)
+        })
+      }
+
+      insertRow = function (entry, timestamp, callback) {
+        index.db.serialize(function () {
+          insertStatement.run(entry.SourceFile, timestamp, JSON.stringify(entry), callback)
+        })
+      }
+    }
+
+    // emit every file in the index
+    function emitRow (row) {
+      emitter.emit('file', {
+        path: row.path,
+        timestamp: new Date(row.timestamp),
+        metadata: JSON.parse(row.metadata)
+      })
+    }
+
+    function init (callback) {
+      if (index.usingBetterSQLite3) {
+        useBetterSQLite()
+      } else if (index.usingSQLite3) {
+        useSQLite()
+      } else {
+        useInMemory()
+      }
+
+      initialise(callback)
+    }
+
+    function finished () {
+      async.series([
+        emitRows,
+        function (callback) {
+          // emit the final count
+          countRows((result) => {
+            emitter.emit('done', { count: result })
+            callback()
           })
         }
-        ++processed
-        emitter.emit('progress', { path: entry.SourceFile, processed: processed, total: toProcess.length })
-      }).on('end', finished)
+      ])
+    }
+
+    async.series([
+      init,
+      // create hashmap of all files in the database
+      function (callback) {
+        // this is not set until init returns
+        populateDatabaseMap(callback)
+      }
+    ],
+    function () {
+      // find all files on disk
+      globber.find(mediaFolder, options, (err, diskMap) => {
+        if (err) return console.error('error', err)
+
+        // calculate the difference: which files have been added, modified, etc
+        const deltaFiles = delta.calculate(databaseMap, diskMap)
+        emitter.emit('stats', {
+          unchanged: deltaFiles.unchanged.length,
+          added: deltaFiles.added.length,
+          modified: deltaFiles.modified.length,
+          deleted: deltaFiles.deleted.length,
+          total: Object.keys(diskMap).length
+        })
+
+        // remove deleted files from the DB
+        deleteRows(deltaFiles.deleted)
+
+        // check if any files need parsing
+        var processed = 0
+        const toProcess = _.union(deltaFiles.added, deltaFiles.modified)
+        if (toProcess.length === 0) {
+          return finished()
+        }
+
+        // call <exiftool> on added and modified files
+        // and write each entry to the database
+        const stream = exiftool.parse(mediaFolder, toProcess, options.concurrency)
+        stream.on('data', entry => {
+          const timestamp = moment(entry.File.FileModifyDate, EXIF_DATE_FORMAT).valueOf()
+
+          insertRow(entry, timestamp)
+
+          ++processed
+          emitter.emit('progress', { path: entry.SourceFile, processed: processed, total: toProcess.length })
+        }).on('end', finished)
+      })
     })
 
     return emitter
